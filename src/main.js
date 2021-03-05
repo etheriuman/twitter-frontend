1c593a226042cc07c22e586cf32a24e7829cd681
etheriuman <timmy26291125@gmail.com>
2021-03-05 14:12:29 +0800

    bootstrap-vue init

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import './assets/application.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faHome, faUser, faCog, faArrowLeft, faComment, faHeart, faFeatherAlt } from '@fortawesome/free-solid-svg-icons'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 library.add(
   faHome,
@@ -18,6 +20,8 @@ library.add(
   )
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
