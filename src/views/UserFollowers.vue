6c2a08a9a28027b0424eba8aa6999b2bb506a6b8
belinda <dog83919814@yahoo.com.tw>
2021-03-05 22:03:13 +0800

    UserFollowers framework

--- src/views/UserFollowers.vue
+++ src/views/UserFollowers.vue
@@ -1,1 +1,108 @@
 // 使用者追隨者頁
+<template>
+  <div class="main">
+    <div class="column-left column">
+      <Navbar @after-submit="handleAfterSubmit" />
+    </div>
+    <div class="column-main column">
+      <div class="card main-content">
+        <ul class="list-group list-group-flush">
+          <PageHead />
+          <UserFollowsNavTabs />
+        </ul>
+      </div>
+    </div>
+    <div class="column-right column">
+      <Recommendation :user-id="userId" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./../components/Navbar";
+import PageHead from "./../components/PageHead";
+import UserFollowsNavTabs from "./../components/UserFollowsNavTabs.vue";
+import Recommendation from "./../components/Recommendation";
+
+export default {
+  name: "UserTweets",
+  components: {
+    Navbar,
+    PageHead,
+    UserFollowsNavTabs,
+    Recommendation,
+  },
+  data() {
+    return {
+      userId: 1,
+    };
+  },
+};
+</script>
+
+<style scoped>
+* {
+  outline: 1px solid pink;
+}
+
+.main {
+  max-height: 100vh;
+  display: grid;
+  grid-template-columns: 250px 1fr 300px;
+  grid-template-areas: "left main right";
+}
+
+.main-content {
+  width: 90%;
+  max-height: 100vh;
+  overflow: auto;
+  /* hide from firefox */
+  scrollbar-width: none;
+}
+
+.main-content::-webkit-scrollbar {
+  /* hide from chrome */
+  display: none;
+}
+
+.column {
+  display: flex;
+}
+
+.column-left {
+  grid-area: left;
+  justify-content: flex-end;
+}
+
+.column-main {
+  grid-area: main;
+  justify-content: center;
+}
+
+.column-right {
+  grid-area: right;
+  justify-content: flex-start;
+}
+
+/* pad 尺寸 */
+@media screen and (max-width: 992px) {
+  .main {
+    grid-template-rows: calc(100% - 50px) 50px;
+    grid-template-columns: 1fr 300px;
+    grid-template-areas:
+      "main right"
+      "left left";
+  }
+}
+
+/* mobile 尺寸 */
+@media screen and (max-width: 768px) {
+  .main {
+    grid-template-rows: calc(100% - 50px) 50px;
+    grid-template-columns: 1fr 300px;
+    grid-template-areas:
+      "main main"
+      "left left";
+  }
+}
+</style>
