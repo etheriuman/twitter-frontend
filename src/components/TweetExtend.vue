1bbbc4f2e2924292ec32762a705466727a837fcc
etheriuman <timmy26291125@gmail.com>
2021-03-06 05:17:39 +0800

    buildup TweetExtend

--- src/components/TweetExtend.vue
+++ src/components/TweetExtend.vue
@@ -2,48 +2,52 @@
 <template>
   <div class="list-group-item">
     <div class="card-body">
-      <div class="card-body-side">
-        <!-- user avatar -->
-        <router-link :to="{name: 'user-tweets', params: {id:tweet.User.id}}">
-          <img class="avatar" :src="tweet.User.avatar" alt="">
-        </router-link>
-      </div>
       <div class="card-body-content">
         <!-- tweetCard header -->
+        <!-- user avatar -->
         <div class="content-header">
-          <span class="user-name">{{tweet.User.name}}</span>
-          <span class="text-muted">{{tweet.User.account}}</span>
-          <span class="text-muted"> &#8231; </span>
-          <span class="text-muted">{{tweet.createdAt | fromNow}}</span>
+          <router-link :to="{name: 'user-tweets', params: {id:tweet.User.id}}">
+            <img class="avatar" :src="tweet.User.avatar" alt="">
+          </router-link>
+          <div class="content-header-description">
+            <p><span class="user-name">{{tweet.User.name}}</span></p>
+            <p><span class="text-muted">{{tweet.User.account}}</span></p>
+          </div>
         </div>
         <!-- tweet description -->
         <div class="content-body">
-          <p>{{tweet.description}}</p>
+          <p class="description">{{tweet.description}}</p>
+          <p>
+            <span class="text-muted">{{tweet.createdAt | time}}</span>
+            <span class="text-muted"> &#8231; </span>
+            <span class="text-muted">{{tweet.createdAt | date}}</span>
+          </p>
         </div>
-        <div class="content-footer">
+        <div class="content-belly">
           <!-- reply button -->
           <!-- dynamic data-target -->
-          <div class="reply">
-            <font-awesome-icon 
-            class="icon" 
-            icon="comment" 
-            data-toggle="modal"
-            :data-target="`#${replyingId}`" 
-            />
-            <span>{{tweet.repliesNumber}}</span>
+            <span>{{tweet.repliesNumber}} <span class="text-muted">回覆</span></span>
+            <span>{{tweet.likesNumber}} <span class="text-muted">喜歡次數</span></span>
           </div>
           <!-- 如果有like就顯示 -->
-          <div class="liked" v-if="tweet.isLiked">
-            <font-awesome-icon class="icon" icon="heart" @click.prevent.stop="deleteLike" />
-            <span>{{tweet.likesNumber}}</span>
-          </div>
-          <!-- 如果沒like就顯示 -->
-          <div class="like" v-else>
-            <font-awesome-icon class="icon" icon="heart" @click.prevent.stop="addLike" />
-            <span>{{tweet.likesNumber}}</span>
+          <div class="content-footer">
+            <div class="reply">
+              <font-awesome-icon 
+              class="icon" 
+              icon="comment" 
+              data-toggle="modal"
+              :data-target="`#${replyingId}`" 
+              />
+            </div>
+            <div class="liked" v-if="tweet.isLiked">
+              <font-awesome-icon class="icon" icon="heart" @click.prevent.stop="deleteLike" />
+            </div>
+            <!-- 如果沒like就顯示 -->
+            <div class="like" v-else>
+              <font-awesome-icon class="icon" icon="heart" @click.prevent.stop="addLike" />
+            </div>
           </div>
         </div>
-      </div>
     </div>
     <Replying :replying-tweet="tweet" :replying-id="replyingId" />
   </div>
@@ -51,7 +55,7 @@
 
 <script>
 import Replying from './../components/Replying'
-import { fromNowFilter } from './../utils/mixins'
+import { fromNowFilter, momentFilter } from './../utils/mixins'
 
 export default {
   components: {
@@ -85,7 +89,7 @@ export default {
       }
     }
   },
-  mixins: [fromNowFilter]
+  mixins: [fromNowFilter, momentFilter]
 }
 </script>
 
@@ -115,19 +119,40 @@ export default {
 
 .content-header {
   margin-bottom: 10px;
+  display: flex;
+}
+
+.content-header p {
+  margin: 0;
 }
 
 .user-name {
   margin-right: 10px;
 }
 
+.description {
+  font-size: 1.5rem;
+}
+
+.content-belly {
+  padding: 15px 0 15px 0;
+  border-top: 1px solid #eeeeee;
+  border-bottom: 1px solid #eeeeee;
+}
+
+.content-belly span span {
+  margin-right: 20px;
+}
+
 .content-footer {
   display: flex;
+  padding-top: 10px;
 }
 
 .icon {
   margin-right: 15px;
   cursor: pointer;
+  font-size: 1.5rem;
 }
 
 .liked {
@@ -135,7 +160,7 @@ export default {
 }
 
 .reply {
-  margin-right: 50px;
+  margin-right: 90px;
 }
 
 /* 統一屬性 */
@@ -144,6 +169,7 @@ export default {
   height: 50px;
   border-radius: 50%;
   object-fit: cover;
+  margin-right: 10px;
 }
 
 
--- src/views/Tweet.vue
+++ src/views/Tweet.vue
@@ -7,8 +7,11 @@
       <div class="card main-content">
         <ul class="list-group list-group-flush">
             <PageHead />
-            <TweetExtend />
-            <ReplyCard />
+            <TweetExtend :initial-tweet="tweet"
+            @after-add-like="handleAfterAddLike"
+            @after-delete-like="handleAfterDeleteLike"
+            />
+            <!-- <ReplyCard /> -->
         </ul>
       </div>
     </div>
@@ -23,57 +26,57 @@ import Navbar from './../components/Navbar'
 import PageHead from './../components/PageHead'
 import Recommendation from './../components/Recommendation'
 import TweetExtend from './../components/TweetExtend'
-import ReplyCard from './../components/ReplyCard'
+// import ReplyCard from './../components/ReplyCard'
 
 const dummyData = {
   tweet: {
-	id: 2, // 推文id
-	description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit voluptatem repellat autem in molestiae saepe.', // 推文內容
-	likesNumber: 23, // 推文like數
-	repliesNumber: 3, // 推文回覆數
-	createdAt: '', // 推文發布時間
-	isLiked: true, // 是否按過like
-	User: { // 推主資料
-		id: 1, // 連結用user.id
-		name: 'Pizza Hut', // 推主名稱
-		account: '@pizzahut', // 推主帳號
-		avatar: 'https://www.meme-arsenal.com/memes/8ab5fe07681cd172915e9472a0a8443d.jpg', // 推主照片
-	},
-	Replies: [ //推文回覆串
-		{
-			id: 5, // 回覆id
-			comment: 'Yeah! I think so', // 回覆內容
-			createdAt: '', // 回覆發布時間
-			User: { // 回覆者資料
-				id: 4, // 連結用user.id
-				name: 'Ricardo Milos', // 回覆者名稱
-				account: '@ricardomilos', // 回覆者帳號
-				avatar: 'https://www.meme-arsenal.com/memes/8ab5fe07681cd172915e9472a0a8443d.jpg' // 回覆者照片
-			}
-		},
-		{
-			id: 3, // 回覆id
-			comment: 'No! I dont think so', // 回覆內容
-			createdAt: '', // 回覆發布時間
-			User: { // 回覆者資料
-				id: 5, // 連結用user.id
-				name: 'Joe Biden', // 回覆者名稱
-				account: '@joebiden', // 回覆者帳號
-				avatar: 'https://www.meme-arsenal.com/memes/8ab5fe07681cd172915e9472a0a8443d.jpg' // 回覆者照片
-			}
-		},
-    {
-			id: 2, // 回覆id
-			comment: 'Well! I dont know', // 回覆內容
-			createdAt: '', // 回覆發布時間
-			User: { // 回覆者資料
-				id: 6, // 連結用user.id
-				name: 'Donald Trump', // 回覆者名稱
-				account: '@donaldtrump', // 回覆者帳號
-				avatar: 'https://www.meme-arsenal.com/memes/8ab5fe07681cd172915e9472a0a8443d.jpg' // 回覆者照片
-			}
-		},
-	]
+    id: 2, // 推文id
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit voluptatem repellat autem in molestiae saepe.333', // 推文內容
+    likesNumber: 23, // 推文like數
+    repliesNumber: 3, // 推文回覆數
+    createdAt: '2011-04-20T09:30:51.01', // 推文發布時間
+    isLiked: true, // 是否按過like
+    User: { // 推主資料
+      id: 1, // 連結用user.id
+      name: 'Pizza Hut', // 推主名稱
+      account: '@pizzahut', // 推主帳號
+      avatar: 'https://www.meme-arsenal.com/memes/8ab5fe07681cd172915e9472a0a8443d.jpg', // 推主照片
+    },
+    Replies: [ //推文回覆串
+      {
+        id: 5, // 回覆id
+        comment: 'Yeah! I think so', // 回覆內容
+        createdAt: '', // 回覆發布時間
+        User: { // 回覆者資料
+          id: 4, // 連結用user.id
+          name: 'Ricardo Milos', // 回覆者名稱
+          account: '@ricardomilos', // 回覆者帳號
+          avatar: 'https://www.meme-arsenal.com/memes/8ab5fe07681cd172915e9472a0a8443d.jpg' // 回覆者照片
+        }
+      },
+      {
+        id: 3, // 回覆id
+        comment: 'No! I dont think so', // 回覆內容
+        createdAt: '', // 回覆發布時間
+        User: { // 回覆者資料
+          id: 5, // 連結用user.id
+          name: 'Joe Biden', // 回覆者名稱
+          account: '@joebiden', // 回覆者帳號
+          avatar: 'https://www.meme-arsenal.com/memes/8ab5fe07681cd172915e9472a0a8443d.jpg' // 回覆者照片
+        }
+      },
+      {
+        id: 2, // 回覆id
+        comment: 'Well! I dont know', // 回覆內容
+        createdAt: '', // 回覆發布時間
+        User: { // 回覆者資料
+          id: 6, // 連結用user.id
+          name: 'Donald Trump', // 回覆者名稱
+          account: '@donaldtrump', // 回覆者帳號
+          avatar: 'https://www.meme-arsenal.com/memes/8ab5fe07681cd172915e9472a0a8443d.jpg' // 回覆者照片
+        }
+      }
+    ]
   } 
 }
 
@@ -83,47 +86,35 @@ export default {
     PageHead,
     Recommendation,
     TweetExtend,
-    ReplyCard
+    // ReplyCard
   },
   data() {
     return {
-      tweets: []
+      tweet: []
     }
   },
   methods: {
-    fetchTweets() {
-      this.tweets = dummyData.tweets
-    },
     handleAfterSubmit() {
-      // 因為需要取得正確createdAt所以選擇重新fetch一次
-      console.log('refetch')
-      this.fetchTweets()
+      console.log('new tweet uploaded')
     },
-    handleAfterAddLike(tweetId) {
+    fetchTweet() {
+      this.tweet = dummyData.tweet
+    },
+    handleAfterAddLike() {
       console.log('add like')
-      this.tweets = this.tweets.map(tweet => {
-        if (tweet.id === tweetId) {
-          return {
-            ...tweet,
-            isLiked: true,
-            likesNumber: tweet.likesNumber + 1
-          }
-        }
-        return tweet
-      })
+      this.tweet = {
+        ...this.tweet,
+        isLiked: true,
+        likesNumber: this.tweet.likesNumber + 1
+      }
     },
-    handleAfterDeleteLike(tweetId) {
+    handleAfterDeleteLike() {
       console.log('delete like')
-      this.tweets = this.tweets.map(tweet => {
-        if (tweet.id === tweetId) {
-          return {
-            ...tweet,
-            isLiked: false,
-            likesNumber: tweet.likesNumber - 1
-          }
-        }
-        return tweet
-      })
+      this.tweet = {
+        ...this.tweet,
+        isLiked: false,
+        likesNumber: this.tweet.likesNumber - 1
+      }
     },
     handleAfterReply(replyData) {
       console.log('replied')
@@ -142,7 +133,7 @@ export default {
     }
   },
   created() {
-    this.fetchTweets()
+    this.fetchTweet()
   }
 }
 </script>
@@ -150,11 +141,11 @@ export default {
 <style scoped>
 
 * {
-  /* outline: 1px solid pink; */
+  outline: 1px solid pink;
 }
 
 .main {
-  max-height: 100vh;
+  height: 100vh;
   display: grid;
   grid-template-columns: 220px 1fr 300px;
   grid-template-areas: "left main right" ;
