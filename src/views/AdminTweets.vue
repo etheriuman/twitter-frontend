f0628ad7dfe894d0c143fe53653f0f196bf3e90f
etheriuman <timmy26291125@gmail.com>
2021-03-06 19:02:51 +0800

    complete AdminTweets & AdminNavbar

--- src/views/AdminTweets.vue
+++ src/views/AdminTweets.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <div class="column-left column">
-      <Navbar />
+      <AdminNavbar />
     </div>
     <div class="column-main column">
       <div class="card main-content">
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-import Navbar from './../components/Navbar'
+import AdminNavbar from './../components/AdminNavbar'
 import PageHead from './../components/PageHead'
 import TweetCard from './../components/TweetCard'
 
@@ -172,7 +172,7 @@ const dummyData = {
 
 export default {
   components: {
-    Navbar,
+    AdminNavbar,
     PageHead,
     TweetCard
   },
@@ -235,19 +235,7 @@ export default {
   justify-content: center;
 }
 
-/* pad 尺寸 */
-@media screen and (max-width: 992px) {
-  
-.main {
-  grid-template-rows: calc(100% - 50px) 50px;
-  grid-template-columns: 1fr;
-  grid-template-areas:
-    "main main"
-    "left left";
-}
-
-}
-
+/* pad 尺寸不變 */
 /* mobile 尺寸 */
 @media screen and (max-width: 768px) {
 
