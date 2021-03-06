ef86349745157055964cf14957c542d86174c7e1
belinda <dog83919814@yahoo.com.tw>
2021-03-06 21:57:50 +0800

    add methods submitSignIn

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,12 +1,14 @@
 // 登入頁
 <template>
- <div class="container py-5">
-    <form class="w-50 mx-auto">
+  <div class="container py-5">
+    <form @submit.prevent.stop="submitSignIn" class="w-50 mx-auto">
       <div class="text-center mb-4">
-        <img src="https://avatars.githubusercontent.com/u/8667311?s=200&v=4" class="twitterLogo mb-3" alt="twitterLogo">
-        <h1 class="title mb-3 font-weight-bold">
-          登入 Alphitter
-        </h1>
+        <img
+          src="https://avatars.githubusercontent.com/u/8667311?s=200&v=4"
+          class="twitterLogo mb-3"
+          alt="twitterLogo"
+        />
+        <h1 class="title mb-3 font-weight-bold">登入 Alphitter</h1>
       </div>
 
       <div class="form-label-group mb-2 text-muted">
@@ -20,7 +22,7 @@
           autocomplete="current-account"
           required
           autofocus
-        >
+        />
       </div>
 
       <div class="form-label-group mb-4 text-muted">
@@ -33,13 +35,10 @@
           class="form-control h-25 p-0"
           autocomplete="current-password"
           required
-        >
+        />
       </div>
 
-      <button
-        class="btn btn-lg btn-primary btn-block mb-3"
-        type="submit"
-      >
+      <button class="btn btn-lg btn-primary btn-block mb-3" type="submit">
         登入
       </button>
 
@@ -52,31 +51,34 @@
           <router-link to="/admin" class="adminSignin">後台登入</router-link>
         </span>
       </div>
-
     </form>
   </div>
-    
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      account: '',
-      password: ''
-    }
-  }
-}
+      account: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitSignIn() {
+      //串接api傳送資料
+      console.log(`account:${this.account}, password:${this.password}`);
+    },
+  },
+};
 </script>
 
 
 <style scoped>
-
 .title {
-  font-size: 23px
+  font-size: 23px;
 }
 label {
-  font-size:15px;
+  font-size: 15px;
   color: #657786;
 }
 .twitterLogo {
@@ -84,25 +86,28 @@ label {
   height: 50px;
 }
 .form-control {
-  border: 0px ;
+  border: 0px;
   border-bottom: 1px solid;
-  border-radius: 0 ;
+  border-radius: 0;
 }
 .btn-primary {
   color: #fff;
-  background-color:  #FF6600;
-  border-color: #FF6600;
+  background-color: #ff6600;
+  border-color: #ff6600;
   border-radius: 90px;
 }
-.btn-primary:hover, .btn-primary:focus, .btn-primary:active:hover{
+.btn-primary:hover,
+.btn-primary:focus,
+.btn-primary:active:hover {
   color: #fff;
-  background-color: #CC5200;
-  border-color: #CC5200;
+  background-color: #cc5200;
+  border-color: #cc5200;
   border-radius: 90px;
 }
-.signup, .dot, .adminSignin{
-  color: #0099FF;;
+.signup,
+.dot,
+.adminSignin {
+  color: #0099ff;
 }
-
 </style>
 
