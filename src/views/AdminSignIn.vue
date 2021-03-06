651b15464263f96db7f0c3b4d94fb4add855816f
belinda <dog83919814@yahoo.com.tw>
2021-03-06 22:34:13 +0800

    add methods AdminSignIn

--- src/views/AdminSignIn.vue
+++ src/views/AdminSignIn.vue
@@ -3,10 +3,12 @@
   <div class="container py-5">
     <form class="w-50 mx-auto">
       <div class="text-center mb-4">
-        <img src="https://avatars.githubusercontent.com/u/8667311?s=200&v=4" class="twitterLogo mb-3" alt="twitterLogo">
-        <h1 class="title mb-3 font-weight-bold">
-          後台登入
-        </h1>
+        <img
+          src="https://avatars.githubusercontent.com/u/8667311?s=200&v=4"
+          class="twitterLogo mb-3"
+          alt="twitterLogo"
+        />
+        <h1 class="title mb-3 font-weight-bold">後台登入</h1>
       </div>
 
       <div class="form-label-group mb-2 text-muted">
@@ -20,7 +22,7 @@
           autocomplete="admin-account"
           required
           autofocus
-        >
+        />
       </div>
 
       <div class="form-label-group mb-4 text-muted">
@@ -33,13 +35,10 @@
           class="form-control h-25 p-0"
           autocomplete="admin-password"
           required
-        >
+        />
       </div>
 
-      <button
-        class="btn btn-lg btn-primary btn-block mb-3"
-        type="submit"
-      >
+      <button class="btn btn-lg btn-primary btn-block mb-3" type="submit">
         登入
       </button>
 
@@ -48,31 +47,36 @@
           <router-link to="/signin" class="signin">前台登入</router-link>
         </span>
       </div>
-
     </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AdminSignIn',
+  name: "AdminSignIn",
   data() {
     return {
-      account: '',
-      password: '',
-    }
-  }
-}
+      account: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitAdminSignIn() {
+      //串接api傳送資料
+      console.log(`account:${this.account}, password:${this.password}`);
+    },
+  },
+};
 </script>
 
 
 
 <style scoped>
 .title {
-  font-size: 23px
+  font-size: 23px;
 }
 label {
-  font-size:15px;
+  font-size: 15px;
   color: #657786;
 }
 .twitterLogo {
@@ -80,24 +84,26 @@ label {
   height: 50px;
 }
 .form-control {
-  border: 0px ;
+  border: 0px;
   border-bottom: 1px solid;
-  border-radius: 0 ;
+  border-radius: 0;
 }
 .btn-primary {
   color: #fff;
-  background-color:  #FF6600;
-  border-color: #FF6600;
+  background-color: #ff6600;
+  border-color: #ff6600;
   border-radius: 90px;
 }
-.btn-primary:hover, .btn-primary:focus, .btn-primary:active:hover{
+.btn-primary:hover,
+.btn-primary:focus,
+.btn-primary:active:hover {
   color: #fff;
-  background-color: #CC5200;
-  border-color: #CC5200;
+  background-color: #cc5200;
+  border-color: #cc5200;
   border-radius: 90px;
 }
-.signin{
-  color: #0099FF;;
+.signin {
+  color: #0099ff;
 }
 </style>
 
