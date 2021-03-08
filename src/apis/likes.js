a3852bf07702c56d6d10007f9a11ae5bbed54cb2
etheriuman <timmy26291125@gmail.com>
2021-03-08 18:11:00 +0800

    update likes.js

--- src/apis/likes.js
+++ src/apis/likes.js
@@ -2,7 +2,7 @@ import { apiHelper } from './../utils/helpers.js'
 
 export default {
   addLike({ tweetId }) {
-    return apiHelper.post(`/like/${tweetId}`, null)
+    return apiHelper.post(`/tweets/${tweetId}/like`, null)
   },
   cancelLike({ tweetId }) {
     return apiHelper.post(`/tweets/${tweetId}/unlike`, null)
