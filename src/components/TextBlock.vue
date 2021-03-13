5fd01b248f49dfd6da639618d3e8333e29ac13dd
etheriuman <timmy26291125@gmail.com>
2021-03-13 21:00:12 +0800

    modify css

--- src/components/TextBlock.vue
+++ src/components/TextBlock.vue
@@ -5,8 +5,13 @@
       <p class="text-muted text-sm">{{ createdAt | time }}</p>
     </div>
     <div v-if="type === 'other'" class="bubble bubble-other">
-      <p class="message message-other">{{ message }}</p>
-      <p class="text-muted text-sm">{{ createdAt | time }}</p>
+      <div class="bubble-other-left">
+          <img :src="userAvatar | emptyImage" alt="user-avatar">
+      </div>
+      <div class="bubble-other-right">
+        <p class="message message-other">{{ message }}</p>
+        <p class="text-muted text-sm">{{ createdAt | time }}</p>
+      </div>
     </div>
     <div v-if="type === 'system'" class="bubble bubble-system">
       <p class=" message message-system">{{ message }}</p>
@@ -15,45 +20,47 @@
 </template>
 
 <script>
-import { momentFilter } from './../utils/mixins'
+import { momentFilter, emptyImageFilter } from './../utils/mixins'
 
 export default {
   data() {
     return {
-      type: 'self', // or 'other' or 'system'
-      message: '123123123',
+      type: 'other', // or 'other' or 'system'
+      message: '12312312112341234',
       createdAt: '',
-      userName: ''
+      userName: '',
+      userAvatar: ''
     }
   },
-  mixins: [momentFilter]
+  mixins: [momentFilter, emptyImageFilter]
 }
 </script>
 
 <style scoped>
   * {
-    /* outline: 1px solid pink; */
+    outline: 1px solid pink;
   }
 
   .text-block {
+    display: flex;
     width: 100%;
     min-height: 30px;
     padding: 10px 20px;
     margin-top: 25px;
   }
   .bubble {
-    max-width: 100%;
+    width: 100%;
     display: flex;
-    flex-flow: column;
+    padding-bottom: 15px;
   }
   .bubble-self {
-    align-items: flex-end;
+    justify-content: flex-end;
+    position: relative;
   }
-  .bubble-other {
-    align-items: flex-start;
-  }
-  .bubble-system {
-    align-items: center;
+  .bubble-self .text-sm {
+    position: absolute;
+    right: 0;
+    bottom: -5px;
   }
   .message {
     max-width: 60%;
@@ -62,13 +69,42 @@ export default {
   }
   .message-self {
     background: #ff6600;
-    color: white;
     border-radius: 15px 15px 0 15px;
+    color: white;
+  }
+  .bubble-other {
+    justify-content: flex-start;
+    align-items: flex-end;
+    max-width: 70%;
+  }
+  .bubble-other-left {
+    padding-bottom: 15px;
+    margin-right: 10px;
+  }
+  .bubble-other-left img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+  .bubble-other-right {
+    position: relative;
+    padding-bottom: 15px;
+    max-width: calc(100% - 50px);
   }
   .message-other {
-    background: #eeeeee;
-    color: #222222;
+    max-width: 100%;
+    background: #dedede;
     border-radius: 15px 15px 15px 0;
+    color: #222222;
+  }
+  .bubble-other-right .text-sm {
+    position: absolute;
+    left: 0;
+    bottom: -5px;
+  }
+  .bubble-system {
+    justify-content: center;
+    align-items: center;
   }
   .message-system {
     background: #eeeeee;
