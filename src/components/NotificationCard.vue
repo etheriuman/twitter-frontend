5db66f86ce624d0e8fddb280dd0a2143b75f1215
etheriuman <timmy26291125@gmail.com>
2021-03-14 00:04:46 +0800

    buildup Notification

---
+++ src/components/NotificationCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="list-group-item">
+    <div class="card-body">
+      <div class="card-top">
+        <router-link :to="{name: 'tweet', params: {id: notification.User.id}}">
+          <img class="user-avatar" :src="notification.User.avatar | emptyImage" alt="user-avatar">
+        </router-link>
+      </div>
+      <div class="card-mid">
+        <p>
+          {{notification.User.name}} 有新的推文通知
+        </p>
+      </div>
+      <div class="card-bot">
+        <p class="description text-muted">
+          {{notification.Tweet.description}}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { emptyImageFilter } from './../utils/mixins'
+
+export default {
+  props: {
+    notification: {
+      type: Object,
+      required: true
+    }
+  },
+  mixins: [emptyImageFilter]
+}
+</script>
+
+<style scoped>
+* {
+  outline: 1px solid pink;
+}
+.card-body {
+  display: flex;
+  flex-flow: column;
+  padding: 5px 10px;
+}
+.card-top {
+  margin-bottom: 10px;
+}
+.user-avatar{
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+}
+.card-mid {
+  margin-bottom: 20px;
+  font-weight: 450;
+}
+.card-bot {
+  margin-bottom: 15px;
+}
+</style>
---
+++ src/views/Notification.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="main">
+    <div class="column-left column">
+      <Navbar />
+    </div>
+    <div class="column-main column">
+      <div class="card main-content">
+        <ul class="list-group list-group-flush">
+          <PageHead />
+          <p class="loading" v-if="isLoading">努力加載中...</p>
+          <NotificationCard v-for="(notification, index) in notifications" :key="index" :notification="notification" />
+        </ul>
+      </div>
+    </div>
+    <div class="column-right column">
+      <Recommendation />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./../components/Navbar"
+import PageHead from "./../components/PageHead"
+import NotificationCard from './../components/NotificationCard'
+import Recommendation from "./../components/Recommendation"
+
+
+export default {
+  components: {
+    Navbar,
+    PageHead,
+    NotificationCard,
+    Recommendation
+  },
+  data() {
+    return {
+      isLoading: false,
+      notifications: [
+        {
+          User: {
+            id: 2,
+            name: 'Yu Wen',
+            avatar: ''
+          },
+          Tweet: {
+            id: 5,
+            description: '爽啊，次啊'
+          }
+        },
+        {
+          User: {
+            id: 3,
+            name: 'Allen',
+            avatar: ''
+          },
+          Tweet: {
+            id: 6,
+            description: '爽啊，次啊'
+          }
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+.main {
+  grid-template-columns: 220px 1fr 300px;
+  grid-template-areas: "left main right";
+}
+
+/* pad 尺寸 */
+@media screen and (max-width: 992px) {
+  .main {
+    grid-template-rows: calc(100% - 50px) 50px;
+    grid-template-columns: 1fr 300px;
+    grid-template-areas:
+      "main right"
+      "left left";
+  }
+}
+
+/* mobile 尺寸 */
+@media screen and (max-width: 768px) {
+  .main {
+    grid-template-rows: calc(100% - 50px) 50px;
+    grid-template-columns: 1fr 300px;
+    grid-template-areas:
+      "main main"
+      "left left";
+  }
+  .column-right {
+    display: none;
+  }
+}
+</style>
